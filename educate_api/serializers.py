f7c0084485d467b1ed650d1aadf78e71b93c7ed8
juliakimchung <julhkm@gmail.com>
2017-04-04 10:04:02 -0500

    APIView used

---
+++ educate_api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from . import models
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		extra_kwargs = {
+			"email": {'write_only': True}
+		}
+		fields = (
+			'id',
+			'course',
+			'name',
+			'email',
+			'review',
+			'rating',
+			'created_at'
+		)
+		model = models.Review
+
+class CourseSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = models.Course
